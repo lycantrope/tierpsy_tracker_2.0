a054816b78df90a6eea26c9ec6ee62712cdaeb3a
Chung-Kuan Chen <b97b01045@gmail.com>
2025-09-08 14:43:03 +0900

    Add console_script to pyproject.toml

--- setup.py
+++ setup.py
@@ -34,13 +34,4 @@ setup(
     ),
     include_dirs=[np.get_include()],
     cmdclass={"build_ext": CustomBuildExt},
-    entry_points={
-        "console_scripts": [
-            "tierpsynn_process="
-            "tierpsynn.processing.main_process_local:process_main_local",
-            "ProcessLocal="
-            "tierpsynn.processing.processMultipleFilesFun:process_main_file",
-            "train_process=" "tierpsynn.train.train:process_main_train",
-        ]
-    },
 )
