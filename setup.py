a2902899d838f3ea06a818cd14ad5e1080ff046b
Chung-Kuan Chen <b97b01045@gmail.com>
2025-09-14 19:40:34 +0900

    Setup uv project

--- setup.py
+++ setup.py
@@ -26,13 +26,6 @@ class CustomBuildExt(build_ext):
 
 # Configuration for the setup
 setup(
-    name="tierpsynn",
-    version="0.0.1",
-    description="Tool for skeletonising worms",
-    url="",
-    author="Weheliye Hashi",
-    author_email="w.weheliye@ic.ac.uk",
-    license="MIT",
     packages=find_packages(),
     zip_safe=False,
     ext_modules=cythonize(
@@ -41,13 +34,4 @@ setup(
     ),
     include_dirs=[np.get_include()],
     cmdclass={"build_ext": CustomBuildExt},
-    entry_points={
-        "console_scripts": [
-            "tierpsynn_process="
-            "tierpsynn.processing.main_process_local:process_main_local",
-            "ProcessLocal="
-            "tierpsynn.processing.processMultipleFilesFun:process_main_file",
-            "train_process=" "tierpsynn.train.train:process_main_train",
-        ]
-    },
 )
